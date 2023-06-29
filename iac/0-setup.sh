aa717dd2c5804da5fda62caa0dfd850fc52791c7
lcassiano <leandro@wetware.com.br>
2023-06-28 21:00:44 -0300

    reorg files

--- iac/0-setup.sh
+++ iac/0-setup.sh
@@ -3,21 +3,23 @@
 # Load variables
 source 1-apply-variables.sh
 
-if [ $1 = "start" ] ; then
-
-    # Create SSL
-    #bash 2-apply-ssl-settings.sh
+if [ $1 = "install" ] ; then
 
     # Create services
     bash 2-install-cert-manager.sh
-    bash 3-configure-ingress.sh
-    bash 4-install-ingress.sh
-    bash 5-apply-volume.sh
-    bash 6-apply-deploy.sh
-    bash 7-apply-services.sh
+    bash 3-install-ingress.sh
+    bash 4-apply-volume.sh
+
+fi
+
+if [ $1 = "start" ] ; then
+
+    bash 5-apply-deploy.sh
+    bash 6-apply-services.sh
+    bash 7-configure-ingress.sh
 
     echo -e '\033[31;5;7m[  WARN  ] \033[0m' "hostname :  https://$APPHOSTNAME/dashboards";
     echo -e '\033[31;5;7m[  WARN  ] \033[0m' "user : admin";
     echo -e '\033[31;5;7m[  WARN  ] \033[0m' "pass : $ADMIN_PASSWORD"
-    
+
 fi
