ec604cee43f5e91c9c1a42e3cf467ab5865e3adf
lcassiano <leandro@wetware.com.br>
2023-07-12 10:07:27 -0300

    fix setup

--- iac/1-apply-variables.sh
+++
@@ -1,8 +0,0 @@
-# Set default variables
-export APPHOSTNAME=$(/sbin/ifconfig eth0 | awk '/inet / { print $2 }' | sed 's/addr://' | sed -e 's/\./\-/g').ip.linodeusercontent.com
-
-# Generate random password
-export ADMIN_PASSWORD=$(openssl rand -base64 15)
-echo ${ADMIN_PASSWORD} | sudo htpasswd -i -c admin_user admin
-export ADMIN_PASSWORD_BASE64=$(cat admin_user | base64)
-rm -rf admin_user
--- iac/setup.sh
+++ iac/setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-
+# Generate random password
+#export ADMIN_PASSWORD=$(openssl rand -base64 15)
+export ADMIN_PASSWORD=$4
+export OSEMAIL=$2
+export OSUSER=$3
+export APPHOSTNAME=$(/sbin/ifconfig eth0 | awk '/inet / { print $2 }' | sed 's/addr://' | sed -e 's/\./\-/g').ip.linodeusercontent.com
+echo ${ADMIN_PASSWORD} | sudo htpasswd -i -c admin_user ${OSUSER}
+export ADMIN_PASSWORD_BASE64=$(cat admin_user | base64)
+rm -rf admin_user
 # Load variables
 source 1-apply-variables.sh
 
@@ -11,9 +19,9 @@ if [ $1 = "start" ]; then
     kubectl -f deploy.yaml apply
     kubectl -f services.yaml apply
 
-    echo '\033[31;5;7m[  WARN  ] \033[0m' "hostname :  https://$APPHOSTNAME/dashboards" > /dev/ttyS0
-    echo '\033[31;5;7m[  WARN  ] \033[0m' "user : admin" > /dev/ttyS0
-    echo '\033[31;5;7m[  WARN  ] \033[0m' "pass : $ADMIN_PASSWORD" > /dev/ttyS0
+    echo -e '\033[31;5;7m[  WARN  ] \033[0m' "hostname :  https://$APPHOSTNAME/dashboards" > /dev/ttyS0
+    echo -e '\033[31;5;7m[  WARN  ] \033[0m' "user : admin" > /dev/ttyS0
+    echo -e '\033[31;5;7m[  WARN  ] \033[0m' "pass : $ADMIN_PASSWORD" > /dev/ttyS0
 
 fi
 
